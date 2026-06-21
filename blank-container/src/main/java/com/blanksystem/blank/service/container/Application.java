ea2ce2f3baf1cf975337d34ab0ca0432b9f9fb23
Luis Fernando García Q... <105936384+lglabs@users.noreply.github.com>
2026-06-21 17:44:11 +0200

    fix: update import for EntityScan to correct package path

--- blank-container/src/main/java/com/blanksystem/blank/service/container/Application.java
+++ blank-container/src/main/java/com/blanksystem/blank/service/container/Application.java
@@ -3,7 +3,7 @@ package com.blanksystem.blank.service.container;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
